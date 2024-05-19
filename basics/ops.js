2a0016eb6ac1fe3817a5e1399567b126fcead8ad
Ronak Rewar <128736765+ronak1293@users.noreply.github.com>
2024-05-19 10:10:11 +0000

    ops and comps in js of different dt

---
+++ basics/ops.js
@@ -0,0 +1,20 @@
+console.log(typeof("1"+2+2));
+console.log(1+1+"2");
+console.log(+true);
+let a=4
+b=c=a
+console.log(a++);
+
+//part2
+
+//comparisons=>both same data type=>badiya else problem
+
+console.log(null>0)//converts to 0
+console.log(null==0)//not converts to 0,else always converts to number(algorithms)
+console.log(null<=0)
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined<=0)
+
+console.log("2"===2);//strict comparison ,compares dt also
